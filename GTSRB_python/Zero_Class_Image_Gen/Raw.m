638d0b12152597892b121f195c9a47f36cead1eb
FaHa <dev-faha@t-online.de>
2017-10-17 19:15:25 +0200

    removed old file structure
    Former-commit-id: 2eaea8f07d3c0e1ec84576a3a15e1ba92d25299c [formerly 48eb99e38fb9b81f5941c53ebc509f0fff07adf2]
    Former-commit-id: b26528f81df0286b969c05ad6e9870d620ba7090

--- GTSRB_python/Zero_Class_Image_Gen/Raw.m
+++
@@ -1,6 +0,0 @@
-   row=320;  col=240;
-fin=fopen('bild18.raw','r');
-I=fread(fin,row*col,'uint8=>uint8'); 
-Z=reshape(I,[row,col]);
-%Z=Z';
-k=imshow(Z)
--- GTSRB_python/image_split/Resize.m
+++
@@ -1,8 +0,0 @@
-function [ O ] = Resize( pic,n )
-%RESIZE Summary of this function goes here
-%   Detailed explanation goes here
-
-O={imresize(pic{1,1},[n n]),imresize(pic{1,2},[n n]),imresize(pic{1,3},[n n]),imresize(pic{1,4},[n n])};
-
-end
-
