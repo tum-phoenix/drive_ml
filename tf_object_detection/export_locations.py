4a3b1cd5fad116e782ccd92f6d1ef4f659e20aa9
Mykyta Denysov <mykyta.denysov@gmail.com>
2018-12-14 23:22:15 +0100

    Add evaluation of existing datasets

--- tf_object_detection/export_locations.py
+++ tf_object_detection/export_locations.py
@@ -3,7 +3,7 @@
 export_locations = {
 	'GTSDB': '/data/Images/Datasets/GTSDB/GTSDB_carolo.csv',
 	'LISA': '/data/Images/Datasets/LISA/training/LISA_extended_carolo.csv',
-	'LISA_EXTENDED': '/data/Images/Datasets/LISA/training/LISA_extended_filtered.csv',
+	'LISA_EXTENDED': '/data/Images/Datasets/LISA/training/LISA_extended_filtered_carolo.csv',
 	'BTSD_TESTING': '/data/Images/Datasets/BTSDB/BelgiumTSD_annotations/BTSD_testing_GTclear_carolo.csv',
 	'BTSD_TRAINING': '/data/Images/Datasets/BTSDB/BelgiumTSD_annotations/BTSD_training_GTclear_carolo.csv',
 	'STS': '/data/Images/Datasets/STS/annotations_2_carolo.csv'
