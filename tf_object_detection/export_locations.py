010b6861615b31870b9ed83e74d0249277bc37d7
Mykyta Denysov <mykyta.denysov@gmail.com>
2018-12-14 14:05:22 +0100

    Add some more data evaluation

---
+++ tf_object_detection/export_locations.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+export_locations = {
+	'GTSDB': '/data/Images/Datasets/GTSDB/GTSDB_carolo.csv',
+	'LISA': '/data/Images/Datasets/LISA/training/LISA_extended_carolo.csv',
+	'LISA_EXTENDED': '/data/Images/Datasets/LISA/training/LISA_extended_filtered.csv',
+	'BTSD_TESTING': '/data/Images/Datasets/BTSDB/BelgiumTSD_annotations/BTSD_testing_GTclear_carolo.csv',
+	'BTSD_TRAINING': '/data/Images/Datasets/BTSDB/BelgiumTSD_annotations/BTSD_training_GTclear_carolo.csv',
+	'STS': '/data/Images/Datasets/STS/annotations_2_carolo.csv'
+}
