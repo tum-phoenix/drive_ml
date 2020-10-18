1fdf31a8a044674f5750b68c533e302556d748ed
Christiaan <christiaan90@hotmail.com>
2020-10-18 10:02:05 +0200

    test

--- LinePolynomialFromSegmentation/CreateLanePolynomials.py
+++ LinePolynomialFromSegmentation/CreateLanePolynomials.py
@@ -11,7 +11,7 @@ IMAGES_RGB = 'images_simulation/rgb'
 IMAGES_LANE = 'images_simulation/lane_segmentation'
 image_rgb = IMAGES_RGB + '/' + image_name
 image_lane = IMAGES_LANE + '/' + image_name
-thr_lane = 5000
+thr_lane = 5001
 
 def get_lanes(img,min_area):
     # default return
@@ -22,14 +22,14 @@ def get_lanes(img,min_area):
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for i in contours:
         contours_poly_tmp = cv.approxPolyDP(i, 2, True)
-        
+
         contours_poly = []
         area = cv.contourArea(contours_poly_tmp)
         if (area>min_area):
             #print("new")
             for j in range(len(contours_poly_tmp)):
                 contours_poly.append(contours_poly_tmp[j][0])
-            contours_poly_total.append(np.array(contours_poly)) 
+            contours_poly_total.append(np.array(contours_poly))
     return contours_poly_total
 
 # load the image
@@ -41,7 +41,7 @@ imread_image_lane = cv.imread(image_lane)
 imread_image_lane_rgb = cv.cvtColor(imread_image_lane, cv.COLOR_BGR2RGB)
 
 # plot the image
-i = 0; 
+i = 0;
 plt.subplot(1,3,i+1),plt.imshow(imread_image_rgb_rgb,'gray')
 plt.title("original image")
 
@@ -72,4 +72,3 @@ else:
 
 plt.xticks([]),plt.yticks([])
 plt.show()
-
